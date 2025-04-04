e1c0f1954ac687ad840735bdc669b8cf04a60449
PROFF-1 <richmondnyamedor04@gmail.com>
2025-04-04 10:24:29 +0000

    Home Screen Created and Rendered

--- Weather-App/App.js
+++ Weather-App/App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import Home from './Src/Screens/Home'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Building A weather-App</Text>
+    <SafeAreaView style={styles.container}>
+      <Home/>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
